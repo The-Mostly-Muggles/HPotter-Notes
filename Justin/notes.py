bb70de4afe8999e74d9fb2b49db58ec2f5e2d0a1
Justin Fulner <46908534+Han-0@users.noreply.github.com>
2021-02-08 14:13:23 -0700

    Update notes.py

--- Justin/notes.py
+++ Justin/notes.py
@@ -4,18 +4,18 @@ from concurrent.futures import ThreadPoolExecutor
 # as a way of seeing how certain syntax works.
 # The code in this file is not necessarily intended to compile or execute.
 
-# line 21 in https://github.com/drsjb80/HPotter/blob/main/src/ListenThread.py
+# line 21 in .../src/ListenThread.py
 list1 = [1,2,3,4,5]
 list2 = ['hey']
 tst = 3 in list1 and list2[0] #if '3 in list1' is true, then 'tst' = list2[0]
 print(tst)
 
-# line 82 in https://github.com/drsjb80/HPotter/blob/main/src/ListenThread.py
+# line 82 in ../src/ListenThread.py
 list3 = [1,2,3]
 tst = (list3[0],int(list3[2])) #creates tuple
 print(tst)
 
-# line 102 in https://github.com/drsjb80/HPotter/blob/main/src/ListenThread.py
+# line 102 in ../src/ListenThread.py
 i = 0
 with ThreadPoolExecutor(max_workers=4) as executor: #define maximum number of executable threads?
     while True: 
@@ -23,14 +23,14 @@ with ThreadPoolExecutor(max_workers=4) as executor: #define maximum number of ex
         print(i)
         if i > 10000: break
 
-# line 120 https://github.com/drsjb80/HPotter/blob/main/src/ListenThread.py
+# line 120 .../src/ListenThread.py
 '''
 ct = ContainerThread(source, self.connection, self.config) # Create ContainterThread object
 f = executor.submit(ct.start) # (from Python documentation) invoking start() function on a thread 
                               # object will invoke that object's run() function.
 '''
 
-# line 117 https://github.com/drsjb80/HPotter/blob/main/src/ContainerThread.py
+# line 117 .../src/ContainerThread.py
 # client = docker.from_env() # instansiate client... connect using default socket or environment configs
 # line 118 
 # self.container = client.containers.run(self.config['container'], detach=True) # create and run container in background
